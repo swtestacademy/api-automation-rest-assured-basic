6f061a707af0c8ca5f5ac2c184c1add936972816
Onur Baskirt <onur.baskirt@dnata.com>
2021-05-26 15:10:44 +0400

    Minor updates.

--- src/test/java/BasicApiTest.java
+++ src/test/java/BasicApiTest.java
@@ -3,23 +3,17 @@ import org.testng.annotations.Test;
 public class BasicApiTest extends BaseTest{
 
     @Test
-    public void T01_StatusCodeTest() {
-        //Verify the http response status returned. Check Status Code is 200?
+    public void T01_StatusCodeAndGetClientsTest() {
         res = utils.RestAssuredUtil.getResponse("/gen/clients");
-        jp = utils.RestAssuredUtil.getJsonPath(res);
         testUtil.checkStatusIs200(res);
-    }
-
-    @Test
-    public void T02_GetClients() {
-        res = utils.RestAssuredUtil.getResponse("/gen/clients");
         jp = utils.RestAssuredUtil.getJsonPath(res);
         System.out.println(testUtil.getClients(jp));
     }
 
    @Test
-    public void T03_GetAndroidModelPackageOptions() {
+    public void T02_GetAndroidModelPackageOptions() {
        res = utils.RestAssuredUtil.getResponse("/gen/clients/android");
+       testUtil.checkStatusIs200(res);
        jp = utils.RestAssuredUtil.getJsonPath(res);
        System.out.println("Opt: " + jp.get("modelPackage.opt"));
        System.out.println("Description: " + jp.get("modelPackage.description"));
