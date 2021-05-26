cd332bee3c158787642a66c8ffe8692c44d23fab
Onur Baskirt <onur.baskirt@dnata.com>
2021-05-26 15:11:03 +0400

    Indentation update.

--- src/test/java/BasicApiTest.java
+++ src/test/java/BasicApiTest.java
@@ -1,6 +1,6 @@
 import org.testng.annotations.Test;
 
-public class BasicApiTest extends BaseTest{
+public class BasicApiTest extends BaseTest {
 
     @Test
     public void T01_StatusCodeAndGetClientsTest() {
@@ -10,13 +10,13 @@ public class BasicApiTest extends BaseTest{
         System.out.println(testUtil.getClients(jp));
     }
 
-   @Test
+    @Test
     public void T02_GetAndroidModelPackageOptions() {
-       res = utils.RestAssuredUtil.getResponse("/gen/clients/android");
-       testUtil.checkStatusIs200(res);
-       jp = utils.RestAssuredUtil.getJsonPath(res);
-       System.out.println("Opt: " + jp.get("modelPackage.opt"));
-       System.out.println("Description: " + jp.get("modelPackage.description"));
-       System.out.println("Type: " + jp.get("modelPackage.type"));
-   }
+        res = utils.RestAssuredUtil.getResponse("/gen/clients/android");
+        testUtil.checkStatusIs200(res);
+        jp = utils.RestAssuredUtil.getJsonPath(res);
+        System.out.println("Opt: " + jp.get("modelPackage.opt"));
+        System.out.println("Description: " + jp.get("modelPackage.description"));
+        System.out.println("Type: " + jp.get("modelPackage.type"));
+    }
 }
